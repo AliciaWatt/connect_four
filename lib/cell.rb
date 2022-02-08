1803ae5fd34e6229ac49a9dd4e3a6ef54cb88b8e
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-08 13:42:46 -0700

    add cell attributes

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,2 +1,8 @@
-class Cell 
+class Cell
+  attr_reader :type_of_player, :coordinates
+
+  def initialize(type_of_player=nil, coordinates="")
+    @type_of_player = type_of_player
+    @coordinates = coordinates
+  end
 end
