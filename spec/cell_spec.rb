c373d771efce24200bd6a5b316ffdd0bbcfcb70b
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-08 13:38:15 -0700

    test: cell attributes

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,8 +1,15 @@
 require './lib/cell'
 
 RSpec.describe Cell do
+  before(:each) do
+    @cell = Cell.new
+  end
   it 'exists' do
-    cell = Cell.new
-    expect(cell).to be_a(Cell)
+    expect(@cell).to be_a(Cell)
+  end
+
+  it 'has attributes' do
+    expect(@cell.type_of_player).to eq(nil)
+    expect(@cell.coordinates).to be_a(String)
   end
 end
