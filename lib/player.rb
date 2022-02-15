5dd4d313a9bd6201137674596b60cf0b5d0861e3
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-14 20:17:53 -0700

    feat: add player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,16 @@
 class Player
+
+  attr_reader   :cells_range, :name
+
+
+  def initialize(cells_range, name= "Player")
+    @cells_range = cells_range
+    @name = name
+
+  end
+
+  def column_selection
+    puts "Which column would you like to choose next?"
+    puts "Column" + "#{column}" + ", great choice."
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,23 @@
-# require './lib/board'
+require './lib/board'
 require './lib/computer'
 require './lib/game'
 require './lib/player'
 
 RSpec.describe Player do
   before(:each) do
+    @board = Board.new
     @player = Player.new
   end
   it 'exists' do
     expect(@player).to be_a Player
+  end
+
+  it 'has attributes' do
+    expect(player.name).to eq "Player"
+    expect(player.column_choice).to eq("")
+  end
+
+  it 'can place a piece in the choosen column'do
+    expect(@board.cells["A1"]).to eq("X")
   end
 end
