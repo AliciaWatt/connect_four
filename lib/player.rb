dcaf42c82c330b626ebeae3a470b7927772939a5
Sherman Au <shermau13@gmail.com>
2022-02-16 18:36:36 -0700

    refactor: player.rb and player_spec.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,40 +1,26 @@
 class Player
-  attr_reader :name, :piece
+  attr_accessor :type
+  attr_reader :name, :piece, :type
 
-  def initialize(name = "Player", type)
+  def initialize(name, type)
     @name = name
     @type = type
     @piece = piece
   end
 
-
-def places_piece(letter)
-  gets player_input.chomp.upcase
-end
-
-def type
-  if player = human_player
-   :human
-  elsif player = computer
-   :computer
-  end
-end
-
-def render
+def piece
   if type == :computer
     'O'
   elsif type == :human
     'X'
-  else
-    '.'
   end
 end
 
-def piece
+def input
   if type == :computer
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     letters.sample
-  elsif type = :human
+  elsif type == :human
     gets.chomp.upcase.to_str
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,16 +4,21 @@ require './lib/player'
 
 RSpec.describe Player do
   before(:each) do
-    @board = Board.new
-    @player = Player.new
+    @player_1= Player.new("Player", :human)
+    @player_2 = Player.new("Roboto", :computer)
   end
   it 'exists' do
-    expect(@player).to be_instance_of Player
+    expect(@player_1).to be_instance_of Player
   end
 
   it 'has attributes' do
-    @player_1 = Player.new
     expect(@player_1.name).to eq("Player")
     expect(@player_1.piece).to eq("X")
+  end
+
+  it 'can tell computer player' do
+
+  expect(@player_2.name).to eq("Roboto")
+  expect(@player_2.piece).to eq("O")
   end
 end
